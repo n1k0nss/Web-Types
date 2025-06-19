8332701e86f4e40663183d24610a8a834b39aca0
n1k0nss <68082134+n1k0nss@users.noreply.github.com>
2025-06-19 12:48:46 +0300

    Update WordsMostCommon

--- src/constants/WordsMostCommon.js
+++ src/constants/WordsMostCommon.js
@@ -1,15 +1,18 @@
 import UkrainianWords from '../assets/Vocab/UkrainianWords.json';
+import UkrainianCommon from '../assets/Vocab/UkrainianCommon.json';
 import EnglishMostFrequentWords from '../assets/Vocab/EnglishMostFrequentWords.json';
 // import Chinese1500IdiomsPinyin from '../assets/Vocab/Chinese1500IdiomsPinyin.json';
 
 
 const COMMON_WORDS = EnglishMostFrequentWords;
 const COMMON_UKRAINIAN_WORDS = UkrainianWords;
+const DEFAULT_UKRAINIAN_WORDS = UkrainianCommon;
 // const COMMON_CHINESE_IDIOMS_WORDS = Chinese1500IdiomsPinyin;
 
 
 export {
     COMMON_WORDS,
     COMMON_UKRAINIAN_WORDS,
+    DEFAULT_UKRAINIAN_WORDS,
     // COMMON_CHINESE_IDIOMS_WORDS
 }
